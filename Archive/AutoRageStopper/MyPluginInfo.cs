32b8e3708e448910291b5c63729c46bf40581738
Simon <79774785+Tashi-Shiro@users.noreply.github.com>
2026-07-17 15:51:15 -0700

    restore archived mods

---
+++ Archive/AutoRageStopper/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace AutoRageStopper;
+
+public static class MyPluginInfo
+{
+    public const string PLUGIN_GUID = "AutoRageStopper";
+    public const string PLUGIN_NAME = "AutoRageStopper";
+    public const string PLUGIN_VERSION = "1.0.0";
+    public const string PLUGIN_AUTHOR = "Simon";
+}
